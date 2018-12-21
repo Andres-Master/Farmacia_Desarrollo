47e0de49d1d575d8c6fc7f8eed57bf96f72d12df
josel <josel@BIGGI>
2018-12-21 16:06:12 -0500

    Desarrollo Proyecto Farmacia, primer paso

--- src/sistemafarmacia/main.java
+++ src/sistemafarmacia/main.java
@@ -7,7 +7,7 @@ public class main {
 
     public static void main(String[] args) {
         new FrmLogin().setVisible(true);
-//        new FrmPrincipal(new Usuario()).setVisible(true);
+        System.out.println("Vamos ...");
     }
     
 }
